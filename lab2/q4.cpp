e65664885d8089af4898da2666fb641ba283fb35
abeeha binte aamer <abeehakhan50@gmail.com>
2025-08-29 12:40:35 +0500

    Create q4.cpp

---
+++ lab2/q4.cpp
@@ -0,0 +1,6 @@
+/*Create a dynamic program that manages monthly expenses for a family.
+=> Use a 1D dynamic array to store expenses for n months.
+=> Add a feature to resize the array if the user wants to track more months later.
+=> Compute and display the total and average expenses.*/
+
+//code 
