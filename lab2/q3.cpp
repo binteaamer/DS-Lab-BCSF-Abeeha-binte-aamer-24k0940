b66e9fecf9010087203f1870f1f5cc548dcde796
abeeha binte aamer <abeehakhan50@gmail.com>
2025-08-29 12:46:19 +0500

    Update q3.cpp

--- lab2/q3.cpp
+++ lab2/q3.cpp
@@ -1,42 +1,43 @@
-/*Task #3:
-Design a program to handle marks of students in different courses using a jagged array
-(since each student is enrolled in a different number of courses).
-=> Input the number of courses each student takes.
-=> Dynamically allocate memory for each student accordingly.
-=> Calculate and display each student’s average marks.*/
-
-//code
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
 int main() {
-  int students;
-  cout<<"enter number of students"<<endl;
-  cin>> students;
- 
-  int**marks=new int*[students];
-  int courses;
-  for(int i=0; i<students; i++){
-      cout<<"enter courses for student "<<i<<endl;
-      cin>>courses;
-      marks[i]=new int[courses];
-      
-  }
-  for(int i=0; i < students;i++){
-      for(int j=0; j<courses; j++){
-          cout<<"enter marks of student "<<i<<" course "<<j<<endl;
-          cin>>marks[i][j];
-          
-      }
-  }
-  
-  for(int i=0; i < students;i++){
-      for(int j=0; j<courses; j++){
-          cout<<"marks of student "<<i<<" for course "<<j<<" "<<marks[i][j];
-          
-      }
-      cout<<endl;
-  }
+    int students;
+    cout << "Enter number of students: ";
+    cin >> students;
+
+    int** marks = new int*[students];
+    int* courseCount = new int[students];
+
+    for (int i = 0; i < students; i++) {
+        cout << "Enter number of courses for student " << i << ": ";
+        cin >> courseCount[i];
+        marks[i] = new int[courseCount[i]];
+    }
+
+    for (int i = 0; i < students; i++) {
+        for (int j = 0; j < courseCount[i]; j++) {
+            cout << "Enter marks of student " << i << " for course " << j << ": ";
+            cin >> marks[i][j];
+        }
+    }
+
+    for (int i = 0; i < students; i++) {
+        int sum = 0;
+        cout << "Marks of student " << i << ": ";
+        for (int j = 0; j < courseCount[i]; j++) {
+            cout << marks[i][j] << " ";
+            sum += marks[i][j];
+        }
+        float avg = (float)sum / courseCount[i];
+        cout << " | Average: " << avg << endl;
+    }
+
+    for (int i = 0; i < students; i++) {
+        delete[] marks[i];
+    }
+    delete[] marks;
+    delete[] courseCount;
+
     return 0;
 }
