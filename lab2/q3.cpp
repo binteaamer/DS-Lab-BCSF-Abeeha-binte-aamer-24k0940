a745c73f4d41e14aa396d01d9e5ba81408aafd70
abeeha binte aamer <abeehakhan50@gmail.com>
2025-08-29 12:53:59 +0500

    Update q3.cpp

--- lab2/q3.cpp
+++ lab2/q3.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-    int students;
-    cout << "Enter number of students: ";
-    cin >> students;
+int students;
+cout << "Enter number of students: ";
+cin >> students;
 
-    int** marks = new int*[students];
-    int* courseCount = new int[students];
+int** marks = new int*[students];
+int* courseCount = new int[students];
 
-    for (int i = 0; i < students; i++) {
+for (int i = 0; i < students; i++) {
         cout << "Enter number of courses for student " << i << ": ";
         cin >> courseCount[i];
         marks[i] = new int[courseCount[i]];
     }
 
-    for (int i = 0; i < students; i++) {
+for (int i = 0; i < students; i++) {
         for (int j = 0; j < courseCount[i]; j++) {
             cout << "Enter marks of student " << i << " for course " << j << ": ";
             cin >> marks[i][j];
@@ -30,7 +30,7 @@ int main() {
             sum += marks[i][j];
         }
         float avg = (float)sum / courseCount[i];
-        cout << " | Average: " << avg << endl;
+        cout << " Average: " << avg << endl;
     }
 
     for (int i = 0; i < students; i++) {
