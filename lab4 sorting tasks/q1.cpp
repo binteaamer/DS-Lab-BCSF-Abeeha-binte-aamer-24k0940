a32ee77ceef317eabfe9ba6947b579932479d10c
abeeha binte aamer <abeehakhan50@gmail.com>
2025-09-12 11:49:03 +0500

    Update q1.cpp

--- lab4 sorting tasks/q1.cpp
+++ lab4 sorting tasks/q1.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
 
+void bubblesort(int arr[], int n){
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - 1; j++) {
+    if (arr[j] < arr[j + 1]) {
+      int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;}
+}}
+}
+
+int main(){
+    int arr[10]={};
+    
+cout<<"input 10 elements for integer array to sort"<<endl;
+for(int i=0;i<10;i++){
+    cin>>arr[i];
+}
+cout<<"unsorted"<<endl;
+for(int i=0;i<10;i++){
+    cout<<arr[i];
+}
+cout<<" "<<endl;
+
+bubblesort(arr,10);
+cout<<"sorted"<<endl;
+for(int i=0;i<10;i++){
+    cout<<arr[i];
+}
+return 0;
+}
